d8921902ffdfb5cd576abf0ec10af88ef9a34360
Valzhana <112197268+Valzhana@users.noreply.github.com>
2023-10-25 20:24:51 +0300

    Delete practice directory

--- practice/myapp_task5/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('one/', views.one, name='one'),
-    path('two/', views.two, name='two'),
-    path('three/', views.three, name='three'),
-]
--- practice/sem2myapp2/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('author/', views.author_read, name='author'),
-    path('articles/', views.articles_read, name='articles'),
-]
